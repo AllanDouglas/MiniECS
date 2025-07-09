6ffad8c0a0cc56ce7fab68317d60c1eacfc56916
Allan Douglas <allandouglas@Mac-mini-de-Allan.local>
2025-07-09 16:18:55 -0300

    remove many parameters constructor

--- Assets/Scripts/Core/Components/ComponentArchetype.cs
+++ Assets/Scripts/Core/Components/ComponentArchetype.cs
@@ -8,10 +8,8 @@ namespace MiniECS
 
         public readonly uint value;
 
-        public ComponentArchetype(int id, int id1 = 0, int id2 = 0, int id3 = 0, int id4 = 0)
-        {
-            value = (1u << id) | (1u << id1) | (1u << id2) | (1u << id3) | (1u << id4);
-        }
+        public ComponentArchetype(int id) => value = 1u << id;
+        public ComponentArchetype(int id, int id1) => value = 1u << id | 1u << id1;
         public ComponentArchetype(uint value) => this.value = value;
         public ComponentArchetype(ComponentID componentID) : this((int)componentID.value) { }
         public ComponentArchetype(ComponentArchetype archetype) : this(archetype.value) { }

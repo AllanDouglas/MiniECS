eced17471a54e82caf1f437907e803e4322fc3be
Allan Douglas <allandouglas@Mac-mini-de-Allan.local>
2025-07-28 15:02:21 -0300

    fix systems id provider

--- Assets/Scripts/Core/Systems/SystemIDProvider.cs
+++ Assets/Scripts/Core/Systems/SystemIDProvider.cs
@@ -1,7 +1,15 @@
+using UnityEditor;
+
 namespace MiniECS
 {
     internal static class SystemIDProvider
     {
+        [InitializeOnLoadMethod]
+        private static void ResetID()
+        {
+            _NEXT_ID = 0;
+        }
+
         private static byte _NEXT_ID = 0;
         public static int Max => 64;
         public static SystemID Next() => new(_NEXT_ID++);

fa0c5afb001d6e2501bdbb07143d195d385ffe3b
Allan Douglas <allandouglas@Mac-mini-de-Allan.local>
2025-08-10 15:40:09 -0300

    fix EntityBehaviour serialization

--- Assets/Scripts/Behaviours/EntityBehaviour.cs
+++ Assets/Scripts/Behaviours/EntityBehaviour.cs
@@ -3,19 +3,18 @@ using UnityEngine;
 namespace MiniECS
 {
     [RequireComponent(typeof(EntityPrototypeController))]
-    public abstract class EntityBehaviour : MiniECSBehaviour, ISerializationCallbackReceiver
+    public abstract class EntityBehaviour : MiniECSBehaviour
     {
         [SerializeField] private EntityPrototypeController _entityController;
         public EntityPrototypeController EntityController { get => _entityController; set => _entityController = value; }
 
-        void ISerializationCallbackReceiver.OnAfterDeserialize() { }
-
-        void ISerializationCallbackReceiver.OnBeforeSerialize()
+        void OnValidate()
         {
             if (_entityController == null)
             {
-                _entityController = GetComponent<EntityPrototypeController>();
+                _entityController = gameObject.GetComponent<EntityPrototypeController>();
             }
         }
+
     }
 }

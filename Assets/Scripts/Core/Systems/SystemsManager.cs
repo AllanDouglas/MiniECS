71abd442d809bd2ccdfa9cc31b92885341061f84
Allan Douglas <allandouglas@Mac-mini-de-Allan.local>
2025-08-16 13:20:52 -0300

    improve SystemManager

--- Assets/Scripts/Core/Systems/SystemsManager.cs
+++ Assets/Scripts/Core/Systems/SystemsManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine.Assertions;
 
@@ -5,15 +6,42 @@ namespace MiniECS
 {
     public sealed class SystemsManager
     {
-        private readonly List<IUpdateSystem> _systems;
+        private readonly IUpdateSystem[] _systems;
+        private readonly IUpdateSystem[] _fixedTimeSystems;
+
+        private int _systemsCount;
+        private int _fixedTimeSystemsCount;
+
+        public ReadOnlySpan<IUpdateSystem> Systems => _systems.AsSpan(0, _systemsCount);
+        public ReadOnlySpan<IUpdateSystem> FixedTimeSystems => _fixedTimeSystems.AsSpan(0, _fixedTimeSystemsCount);
+
 
         public SystemsManager(int bufferSize = 25)
         {
-            _systems = new(bufferSize);
+            _systems = new IUpdateSystem[bufferSize];
+            _fixedTimeSystems = new IUpdateSystem[bufferSize];
         }
 
-        public void Register(IUpdateSystem system) => _systems.Add(system);
+
+
+        public void Register(IUpdateSystem system, bool runAtFixedUpdate = false)
+        {
+            if (!runAtFixedUpdate)
+            {
+                _systems[_systemsCount++] = system;
+                return;
+            }
+
+            _fixedTimeSystems[_fixedTimeSystemsCount++] = system;
+        }
+
         public TSystem Register<TSystem>(TSystem system) where TSystem : class, IUpdateSystem
+        {
+            Register(system as IUpdateSystem, false);
+            return system;
+        }
+
+        public TSystem Register<TSystem>(TSystem system, bool runAtFixedUpdate) where TSystem : class, IUpdateSystem
         {
             Assert.IsNotNull(system, "system cant be null");
 
@@ -23,7 +51,7 @@ namespace MiniECS
 
         public bool TryGet<TSYSTEM>(out TSYSTEM system) where TSYSTEM : IUpdateSystem
         {
-            for (int i = 0; i < _systems.Count; i++)
+            for (int i = 0; i < _systems.Length; i++)
             {
                 if (typeof(TSYSTEM) == _systems[i].GetType())
                 {
@@ -31,6 +59,16 @@ namespace MiniECS
                     return true;
                 }
             }
+
+            for (int i = 0; i < _fixedTimeSystems.Length; i++)
+            {
+                if (typeof(TSYSTEM) == _fixedTimeSystems[i].GetType())
+                {
+                    system = (TSYSTEM)_fixedTimeSystems[i];
+                    return true;
+                }
+            }
+
             system = default;
             return false;
         }

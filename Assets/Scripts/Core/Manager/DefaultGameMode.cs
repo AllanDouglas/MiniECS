9546e5f842ceb03dfb232d3720833d81d6ffe18f
Allan Douglas <allandouglas@Mac-mini-de-Allan.local>
2025-11-21 13:24:54 -0300

    create DefaultGameMode

---
+++ Assets/Scripts/Core/Manager/DefaultGameMode.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+namespace MiniECS
+{
+    public abstract class DefaultGameMode : IGameMode
+    {
+        public virtual void OnEnable(ECSManager ecs) { }
+        public virtual void Start(ECSManager ecs) { }
+        public virtual void OnDisable(ECSManager ecs) { }
+        public virtual void OnDestroy(ECSManager ecs) { }
+        public virtual void Update(ECSManager ecs)
+        {
+            FrameTime frameTime = new(Time.fixedTime, Time.time);
+
+            for (int i = 0; i < ecs.SystemsManager.Systems.Length; i++)
+            {
+                if (ecs.SystemsManager.Systems[i].Enabled)
+                {
+                    ecs.SystemsManager.Systems[i].Update(in frameTime);
+                }
+            }
+        }
+
+        public virtual void FixedUpdate(ECSManager ecs)
+        {
+            FrameTime frameTime = new(Time.fixedTime, Time.time);
+
+            for (int i = 0; i < ecs.SystemsManager.FixedTimeSystems.Length; i++)
+            {
+                if (ecs.SystemsManager.FixedTimeSystems[i].Enabled)
+                {
+                    ecs.SystemsManager.FixedTimeSystems[i].Update(in frameTime);
+                }
+            }
+        }
+
+        public virtual void LateUpdate(ECSManager ecs)
+        {
+            ecs.EventBus.FlushAll(ecs);
+            ecs.MessageBus.FlushAll();
+        }
+    }
+}
